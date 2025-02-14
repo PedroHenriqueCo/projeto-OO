32bf574e38fa683e6693cd1b4485d65bb22d4176
F0RTAH <232003680@aluno.unb.br>
2025-02-14 14:25:26 -0300

    Update MenuExame.java

--- view/MenuExame.java
+++ view/MenuExame.java
@@ -1,7 +1,7 @@
-package br.com.clinica.view;
+package view;
 
-import br.com.clinica.servicos.*;
-import br.com.clinica.entidades.*;
+import servicos.*;
+import entidades.*;
 import javax.swing.JOptionPane;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
