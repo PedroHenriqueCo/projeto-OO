33812bc6dbaadd1ea213e6bd55817b134654a058
F0RTAH <232003680@aluno.unb.br>
2025-02-14 14:13:53 -0300

    Update MedicoRepository.java

--- repositorios/MedicoRepository.java
+++ repositorios/MedicoRepository.java
@@ -1,6 +1,6 @@
-package br.com.clinica.repositorios;
+package repositorios;
 
-import br.com.clinica.entidades.Medico;
+import entidades.Medico;
 import java.util.*;
 import java.util.stream.Collectors;
 
